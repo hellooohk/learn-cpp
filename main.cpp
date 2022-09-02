ce5741d61d5826d3be7501427cdfafffbcf03ea7
Himanshu Kataria <katariahk03@gmail.com>
2022-09-02 11:37:27 +0530

    Revised OOP

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
-#include<iostream> 
+#include<bits/stdc++.h>
 using namespace std;
+//Classes
+class Person{
+    private:
+        string firstName;
+        string lastName;
+    public:
+        void testMethod();
+        void setName(string firstName,string lastName){
+            this->firstName = firstName;
+            this->lastName = lastName;
+        }
+        string getFullName(){
+            return this->firstName + " " + this->lastName + "\n";
+        }
+};
+void Person:: testMethod() {
+    cout << "Hello, I am test Method";
+}
 int main(){
-    cout << "Basics Revised, nothing fancy found";
+    Person *p = new Person();
+    p->setName("Himanshu","Kataria");
+    cout << p->getFullName() ;
+
 }
